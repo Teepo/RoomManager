d78296c04c9f7997b85a1a5e21c5a1e7e53b1d65
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2023-11-15 00:53:15 +0000

    server > store roomName in player instance

--- src/player.js
+++ src/player.js
@@ -1,10 +1,11 @@
 export class Player {
 
-    constructor({ id, login }) {
+    constructor({ id, login, roomName }) {
         
         this.id         = id;
         this.login      = login;
         this.isReady    = false;
+        this.roomName   = roomName;
         this.customData = {};
     }
 }
