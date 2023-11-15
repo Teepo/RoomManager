d9e5a7dec64e4ad5f0592fd924f597b5fbd3a240
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2023-11-15 00:52:41 +0000

    admin > add overlay to create room with any name

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -3,10 +3,8 @@
 	<v-container class="fill-height">
 		<v-responsive class="d-flex align-center fill-height">
 
-			<v-container>
-				<v-btn class="bg-primary" @click="createRoom">CREATE ROOM</v-btn>
-				<v-btn class="bg-primary ml-5" @click="start">START GAME</v-btn>
-				<v-btn class="bg-primary ml-5" @click="return_to_lobby">RETURN TO LOBBY</v-btn>
+			<v-container class="pl-0 mt-10">
+				<v-btn class="bg-primary" @click="showOverlayNewRoom = true">CREATE ROOM</v-btn>
 			</v-container>
 
 			<div class="text-h6 mb-1">ROOMS</div>
@@ -23,22 +21,24 @@
 						<td>{{ room.name }}</td>
 						<td>{{ room.isStarted }}</td>
 						<td>
+							<v-btn icon="mdi-play" @click="this.startRoom(room.name)"></v-btn>
 							<v-btn icon="mdi-delete" @click="this.deleteRoom(room.name)"></v-btn>
 						</td>
 					</tr>
 				</tbody>
 			</v-table>
 
-			<v-container>
-				<v-btn class="bg-primary" @click="createPlayer">CREATE PLAYER</v-btn>
-				<v-btn class="bg-primary ml-5" @click="deleteAllPlayers">CLEAN PLAYERS</v-btn>
+			<v-container class="pl-0 mt-10">
+				<v-btn class="bg-primary" @click="deleteAllPlayers">CLEAN PLAYERS</v-btn>
 			</v-container>
+
 			<div class="text-h6 mb-1">ALL PLAYERS</div>
 			<v-table class="text-left">
 				<thead>
 					<tr>
 						<th class="text-left">ID</th>
 						<th class="text-left">LOGIN</th>
+						<th class="text-left">ROOM</th>
 						<th class="text-left">IS READY</th>
 						<th class="text-left">CUSTOM DATA</th>
 						<th class="text-left">Actions</th>
@@ -48,6 +48,7 @@
 					<tr v-for="player in players" :key="player.id">
 						<td class="text-left">{{ player.id }}</td>
 						<td>{{ player.login }}</td>
+						<td>{{ player.roomName }}</td>
 						<td>{{ player.isReady }}</td>
 						<td>{{ player.customData }}</td>
 						<td>
@@ -56,6 +57,28 @@
 					</tr>
 				</tbody>
 			</v-table>
+
+			<v-dialog
+				v-model="showOverlayNewRoom"
+				contained
+				persistent
+				class="align-center justify-center"
+			>
+				<v-list>
+
+					<v-btn icon @click="showOverlayNewRoom = false">
+						<v-icon>mdi-close</v-icon>
+					</v-btn>
+
+					<v-sheet width="300" class="mx-auto">
+						<v-form @submit.prevent>
+							<v-text-field v-model="formCreateRoomInputValue" :rules="formCreateRoomRules" label="Room name"></v-text-field>
+							<v-btn type="submit" class="bg-primary mb-10 d-flex w-100" @click="createRoom">Create room</v-btn>
+						</v-form>
+					</v-sheet>
+				</v-list>
+			</v-dialog>
+
 		</v-responsive>
 	</v-container>
 </template>
@@ -72,7 +95,17 @@ export default {
 
 		return {
 			players : [],
-			rooms   : []
+			rooms   : [],
+
+			showOverlayNewRoom : false,
+
+			formCreateRoomInputValue: 'blindtest',
+			formCreateRoomRules: [
+				value => {
+					if (value) return true
+					return 'You must enter a room name.'
+				},
+			]
 		}
 	},
     
@@ -123,21 +156,17 @@ export default {
     },
     methods : {
 
-		start() {
-
-			this.socket.emit('start', {
-                roomName : 'petank'
-            });
-		},
-
 		createRoom() {
 
 			this.socket.emit('createRoom', {
-                roomName : 'petank'
+                roomName : this.formCreateRoomInputValue
             });
+
+			this.showOverlayNewRoom = false;
 		},
 
 		createPlayer() {
+
 			this.socket.emit('joinRoom', {
 				login    : this.generateRandomString(10),
                 roomName : 'petank'
@@ -150,6 +179,13 @@ export default {
 
 		deleteAllPlayers() {
 			this.socket.emit('deleteAllPlayers');
+		},
+
+		startRoom(name) {
+
+			this.socket.emit('start', {
+				roomName : name
+			});
 		},
 
 		deleteRoom(name) {
